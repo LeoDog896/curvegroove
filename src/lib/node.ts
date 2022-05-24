4b6f349a5e27076a493befd63c0d8b0b9c76a7a2
LeoDog896 <LeoDog896@hotmail.com>
2022-05-24 13:39:29 +0000

    Seperate NodeBase from node, button abstraction

--- src/lib/node.ts
+++ src/lib/node.ts
@@ -1,12 +1,14 @@
 export interface Position { x: number; y: number; }
 
-export type Node = {
-  position: Position;
-  selected?: boolean;
-} & (
+export type NodeBase = (
   { type: "text", content: string } |
   { type: "image", src: string, alt: string } |
   { type: "group", nodes: Node[] } |
   { type: "rectangle", width: number, height: number } |
   { type: "oval", width: number, height: number }
 );
+
+export type Node = {
+  position: Position;
+  selected?: boolean;
+} & NodeBase;
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import NodeElem from "$lib/Node.svelte"
-  import type { Node } from "$lib/node";
+  import type { Node, NodeBase } from "$lib/node";
 
   interface Scene {
     nodes: Node[]
@@ -10,38 +10,46 @@
     nodes: []
   }]
 
+  interface Button {
+    name: string;
+    defaultNode: NodeBase
+  }
+
+  const buttons: Button[] = [{
+    name: "Add Rectangle",
+    defaultNode: {
+      type: "rectangle",
+      width: 100,
+      height: 100
+    }
+  }, {
+    name: "Add oval",
+    defaultNode: {
+      type: "oval",
+      width: 100,
+      height: 100
+    }
+  }, {
+    name: "Add Text",
+    defaultNode: {
+      type: "text",
+      content: "Hello World!"
+    }
+  }]
+
   $: activeScene = scenes[0]
 </script>
 <div>
-  <div class="inline p-8 border-gray-300 w-full">
-    <button on:click={() => {
-      activeScene.nodes = [...activeScene.nodes, {
-        type: "rectangle",
-        width: 100,
-        height: 100,
-        position: { x: 0, y: 0 }
-      }]
-    }}>Add Rectangle</button>
-  </div>
-  <div class="inline p-8 border-gray-300 w-full">
-    <button on:click={() => {
-      activeScene.nodes = [...activeScene.nodes, {
-        type: "oval",
-        width: 100,
-        height: 100,
-        position: { x: 0, y: 0 }
-      }]
-    }}>Add Oval</button>
-  </div>
-  <div class="inline p-8 border-gray-300 w-full">
-    <button on:click={() => {
-      activeScene.nodes = [...activeScene.nodes, {
-        type: "text",
-        content: "Hello World!",
-        position: { x: 0, y: 0 }
-      }]
-    }}>Add Text</button>
-  </div>
+  {#each buttons as button}
+    <div class="inline p-8 border-gray-300 w-full">
+      <button on:click={() => {
+        activeScene.nodes = [...activeScene.nodes, {
+          ...button.defaultNode,
+          position: { x: 0, y: 0 }
+        }]
+      }}>{button.name}</button>
+    </div>
+  {/each}
 </div>
 {#each activeScene.nodes as node}
   <NodeElem {node}></NodeElem>
