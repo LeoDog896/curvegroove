01d2d325976e4fc060c542b6f70e272f631c5513
Tristan <LeoDog896@hotmail.com>
2022-05-23 20:03:49 -0400

    Add oval controls

--- src/lib/Node.svelte
+++ src/lib/Node.svelte
@@ -6,15 +6,15 @@
 </script>
 
 {#if node.type == "text"}
-  <p use:nodedrag={node}>{node.content}</p>
+  <p class="fixed" use:nodedrag={node}>{node.content}</p>
 {:else if node.type == "image"}
-  <img use:nodedrag={node} src={node.src} alt={node.alt}/>
+  <img class="fixed" use:nodedrag={node} src={node.src} alt={node.alt}/>
 {:else if node.type == "group"}
   {#each node.nodes as loopNode}
     <svelte:self node={loopNode}/>
   {/each}
 {:else if node.type == "rectangle"}
-  <div use:nodedrag={node} style="width: {node.width}px; height: {node.height}px;" class="bg-gray-500"></div>
+  <div use:nodedrag={node} style="width: {node.width}px; height: {node.height}px;" class="fixed bg-gray-500"></div>
 {:else if node.type == "oval"}
-  <div use:nodedrag={node} style="width: {node.width}px; height: {node.height}px;" class="rounded-full bg-gray-500"></div>
+  <div use:nodedrag={node} style="width: {node.width}px; height: {node.height}px;" class="fixed rounded-full bg-gray-500"></div>
 {/if}
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -12,15 +12,27 @@
 
   $: activeScene = scenes[0]
 </script>
-<div class="p-8 border-gray-300 w-full">
-  <button on:click={() => {
-    activeScene.nodes = [...activeScene.nodes, {
-      type: "rectangle",
-      width: 100,
-      height: 100,
-      position: { x: 0, y: 0 }
-    }]
-  }}>Add Rectangle</button>
+<div>
+  <div class="inline p-8 border-gray-300 w-full">
+    <button on:click={() => {
+      activeScene.nodes = [...activeScene.nodes, {
+        type: "rectangle",
+        width: 100,
+        height: 100,
+        position: { x: 0, y: 0 }
+      }]
+    }}>Add Rectangle</button>
+  </div>
+  <div class="inline p-8 border-gray-300 w-full">
+    <button on:click={() => {
+      activeScene.nodes = [...activeScene.nodes, {
+        type: "oval",
+        width: 100,
+        height: 100,
+        position: { x: 0, y: 0 }
+      }]
+    }}>Add Oval</button>
+  </div>
 </div>
 {#each activeScene.nodes as node}
   <NodeElem {node}></NodeElem>
